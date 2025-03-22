9b02a548eb47c9e287ae934d8ea54f67f7565b68
Ricky-Rubin <owolabi1738@gmail.com>
2025-03-22 06:44:57 +0100

    Created a sub-drop-down within a drop-down

--- src/carousel.js
+++ src/carousel.js
@@ -1,11 +1,21 @@
 const menuTab = document.querySelector('.menu-tab');
 const menuOptions = document.querySelector('.menu-options');
+const homeTab = document.querySelector('.home');
+const contact = document.querySelector('.contact');
 
 function showDrop() {
     menuTab.addEventListener('click', () => {
         menuTab.classList.toggle('click');
         menuOptions.classList.toggle('click');
     });
+
+    homeTab.addEventListener('mouseover', () => {
+        contact.classList.toggle('show');
+    });
+
+    homeTab.addEventListener('mouseout', () => {
+        contact.classList.remove('show');
+    })
 };
 
 export { showDrop }
